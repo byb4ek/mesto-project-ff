0607eacee54cb61d1b5df7e4079fb8f32b0feaa2
byb4ek <157111926+byb4ek@users.noreply.github.com>
2024-04-28 13:57:09 +0300

    first try

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
+const template = document.querySelector("#card-template").content;
+const cardList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function createCard(infoCard, deleteCard) {
+  const card = template.querySelector(".card").cloneNode(true);
+  const cardImage = card.querySelector(".card__image");
+  const cardTitle = card.querySelector(".card__title");
+  const cardDeleteButton = card.querySelector(".card__delete-button");
+  cardImage.alt = infoCard.name;
+  cardImage.src = infoCard.link;
+  cardTitle.textContent = infoCard.name;
+  cardDeleteButton.addEventListener("click", () => {
+    deleteCard(card);
+  });
+  return card;
+}
 
-// @todo: Функция создания карточки
+function deleteCard(card) {
+  card.remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+  const saveCard = createCard(item, deleteCard);
+  cardList.append(saveCard);
+});
