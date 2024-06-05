425d43a592e904ab3fdaac11adc895235a8b1d4c
byb4ek <157111926+byb4ek@users.noreply.github.com>
2024-06-05 14:10:40 +0300

    Ошибка выводится

--- src/componets/validation.js
+++ src/componets/validation.js
@@ -4,14 +4,15 @@ export function clearValidation(){
 }
 
 function showInputError (formElement,inputElement,errorMessage,config){
-	const errorElement = formElement.querySelector(`${inputElement.id}-error`);
+	const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+	console.log(inputElement.id);
 	inputElement.classList.add(config.inputErrorClass);
 	errorElement.textContent = errorMessage;
   errorElement.classList.add(config.errorClass);
 }
 
 function hideInputError(formElement,inputElement,config){
-	const errorElement = formElement.querySelector(`${inputElement.id}-error`);
+	const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
 	inputElement.classList.remove(config.inputErrorClass);
   errorElement.classList.remove(config.errorClass);
 	errorElement.textContent = '';
@@ -20,11 +21,8 @@ function hideInputError(formElement,inputElement,config){
 function checkInputValidity(formElement,inputElement,config){
   if (!inputElement.validity.valid) {
 		//Если наш input не валиден то показываем ошибку
-		//Передаем Форму инпутов,сам инпут и сообщение ошибки
-		console.log(inputElement);
-		console.log(inputElement.validationMessege);
-		
-    showInputError(formElement,inputElement,inputElement.validationMessege,config);
+		//Передаем Форму инпутов,сам инпут и сообщение ошибки		
+    showInputError(formElement,inputElement,inputElement.validationMessage,config);
   } else {
     hideInputError(formElement,inputElement,config);
   }
@@ -37,7 +35,7 @@ const hasInvalidInput = (inputList) => {
   })
 };
 
-const toggleButtonState = (inputList, buttonElement,config) => {
+ const toggleButtonState = (inputList, buttonElement,config) => {
   if (hasInvalidInput(inputList)) {
     buttonElement.classList.add(config.inactiveButtonClass);
   } else {
@@ -47,13 +45,13 @@ const toggleButtonState = (inputList, buttonElement,config) => {
 
 const setEventListeners = (formElement,config) => {
   const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
-	const buttonElement = formElement.querySelector(config.submitButtonSelector);
-	toggleButtonState(inputList,buttonElement,config);
+	//const buttonElement = formElement.querySelector(config.submitButtonSelector);
+	//toggleButtonState(inputList,buttonElement,config);
   inputList.forEach((inputElement) => {
 		//inputElement это наш один текущий инпут 
     inputElement.addEventListener('input', () => {
-      checkInputValidity(formElement, inputElement,config);
-			toggleButtonState(inputList,buttonElement,config);
+      checkInputValidity(formElement, inputElement, config);
+		//	toggleButtonState(inputList,buttonElement,config);
 			//передаем форму и инпут текущий потом будет следующий 
     });
   });
